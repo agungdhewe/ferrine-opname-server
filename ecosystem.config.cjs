8baac5e89de279ef42acf5dd0f23c276c0aa60e4
agungdhewe <agung_dhewe@yahoo.com>
2026-01-21 15:36:51 +0700

    feat: Add PM2 configuration and a run script for process management.

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: "opname-server",
+            script: "src/server.js",
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "300M"
+        }
+    ]
+};
